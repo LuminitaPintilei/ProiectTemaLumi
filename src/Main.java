b780d91e9e5efc1846ca9614714fa82fb4467fc7
Luminita Pintilei <luminitapintilei@gmail.com>
2021-10-25 00:19:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Hello");
+       System.out.println("Luminita");
+       short primulNumar = 3;
+
+       short aldoileaNumar = 5;
+
+       int adunare = primulNumar + aldoileaNumar;
+       System.out.println(adunare);
+
+       short nrPatru = 8;
+
+       short nrCinci = 4;
+
+       int impartire = nrPatru / nrCinci;
+       System.out.println(impartire);
+
+       System.out.println(-5 + 8 * 6);
+
+       int x = 55;
+       int y = 9;
+       int z = x+y;
+       int w = 9;
+
+       System.out.println(z % w);
+
+
+       System.out.println((double)(20 + -3*5 / 8));
+
+       float nrA = (float)((5+15)/(3*2)-8);
+       int nrB = 3;
+       System.out.println(nrA % nrB);
+
+       System.out.println((double)(9 / 4));
+
+       String concluzie = "Nu inteleg de ce nu imi afiseaza numar cu mai multe zecimale la rezultat daca aleg variabila double.";
+       System.out.println(concluzie);
+
+
+
+
+
+
+    }
+}
